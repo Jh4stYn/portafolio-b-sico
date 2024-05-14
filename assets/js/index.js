1decda77926aaaa80b142f44479427fa656d3c4b
Jhastyn Payehuanca Riquelme <jpayehuancar@unsa.edu.pe>
2024-05-13 22:56:58 -0500

    Agregamos la persistencia de datos, iniciammos con la sección hero, y usamos la librería 'Typed'

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,61 @@
-/* ====================== NAVBAR ====================== */
-const menu = document.getElementById('menu')
-const btnOpen = document.getElementById('btnOpen')
-const btnClose = document.getElementById('btnClose')
+// NAVBAR
+const menu = document.getElementById("menu")
+const nav = document.querySelector(".nav")
+const links = nav.querySelectorAll(".menu__link")
 
-function openMenu () {
-  menu.classList.add('show-menu')
+nav.addEventListener("click", function (event) {
+    if(event.target.closest("#btnOpen")) {
+        menu.classList.add("show-menu")
+    }
+    
+    if(event.target.closest("#btnClose")) {
+        menu.classList.remove("show-menu")
+    }
+
+    if (event.target.matches(".menu__link")) {
+        menu.classList.remove("show-menu")
+
+        for(const link of links){
+            link.classList.remove("active")
+        }
+        event.target.classList.add("active")
+    }
+})
+
+// BLACKMODE
+const body = document.body
+const ls = window.localStorage
+const btnTheme = document.getElementById("btnTheme")
+
+const theme = ls.getItem("darkMode")
+const sun ="bx bxs-sun"
+const moon ="bx bxs-moon"
+
+if(theme) {
+    body.classList.add("dark")
+    
+} else {
+    body.classList.remove("dark")
 }
 
-function closeMenu () {
-  menu.classList.remove('show-menu')
-}
+btnTheme.addEventListener("click", function () {   
+    body.classList.toggle("dark")
 
-btnOpen.onclick = openMenu
-btnClose.onclick = closeMenu
-/* =================== Other JS =================== */
+    if(body.classList.contains("dark")) {
+        ls.setItem("darkMode", true)
+        btnTheme.firstElementChild.className = sun
+    }else {
+        ls.removeItem("darkMode")
+        btnTheme.firstElementChild.className = moon
+    }
+})
+
+// Typing
+var typed = new Typed('#typing', {
+    strings: ['Frontend Developer', 'Web Developer', 'Designer', 'Fullstack Developer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    startDelay: 25,
+    loop: true,
+    loopCount: Infinity,
+});
